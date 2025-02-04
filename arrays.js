36d59a0003cb5580adaf460ebb7f152f2640813a
Jan-C-Nielsen <U2488247@rts-365.dk>
2025-02-04 10:12:44 +0100

    8. opg

--- arrays.js
+++ arrays.js
@@ -5,5 +5,28 @@ document.addEventListener("DOMContentLoaded", function() {
     let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 
     // --- Løs opgaverne herunder --- 
+    let newNumbers = numbersArray.filter(x => { if(x > 20) { return x; }});
+    console.log("Opgave 1", newNumbers)
 
+    catsArray.push("Sniffles");
+    console.log("Opgave 2", catsArray);
+
+    console.log("Opgave 3a", dogsArray.includes("Trixie"));
+
+    console.log("Opgave 3b", dogsArray.includes("Baxter"));
+
+    console.log("Opgave 4a", catsArray.includes("Bagheera"));
+
+    console.log("Opgave 4b", catsArray.includes("Salem"));
+
+    let numbersArray3 = numbersArray.map(x => x*3);
+    console.log("Opgave 5", numbersArray3);
+
+    console.log("Opgave 6", dogsArray.join(""));
+
+    console.log("Opgave 7a", fruitsArray.findIndex(x => x==="Mango"));
+
+    console.log("Opgave 7b", fruitsArray.findIndex(x => x==="Blåbær"));
+
+    console.log("Opgave 8", dogsArray.filter(x => x!=="Polly"));
 }) // ends DOMContentLoaded
